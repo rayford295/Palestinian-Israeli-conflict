87d0a6a7d887c9904ab798b9586b91a5442fdffc
rayford295 <130307030+rayford295@users.noreply.github.com>
2023-12-04 23:30:42 -0800

    Add files via upload
    12.4 latest

--- get_data.py
+++ get_data.py
@@ -9,17 +9,17 @@ Original file is located at
 
 import requests
 from bs4 import BeautifulSoup
+import xml.etree.ElementTree as ET
 import csv
 
+# 函数：爬取维基百科页面
 def scrape_wikipedia_page(url, file_name):
     response = requests.get(url)
     if response.status_code != 200:
-        print(f"请求失败，状态码：{response.status_code}")
+        print(f"请求维基百科页面失败，状态码：{response.status_code}")
         return
 
     soup = BeautifulSoup(response.content, 'html.parser')
-
-    # 找到所有标题和段落
     elements = soup.find_all(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p'])
 
     with open(file_name, 'w', newline='', encoding='utf-8') as file:
@@ -31,16 +31,49 @@ def scrape_wikipedia_page(url, file_name):
             element_content = element.get_text().strip()
             writer.writerow([element_type, element_content])
 
-    print(f"内容已保存到 {file_name}")
+    print(f"维基百科内容已保存到 {file_name}")
 
-# 爬取英文版本的页面
+# 函数：从Google News RSS feed获取并保存新闻
+def save_news_to_csv(rss_url, csv_filename):
+    response = requests.get(rss_url)
+    if response.status_code != 200:
+        print(f"获取Google News数据失败，状态码：{response.status_code}")
+        return
+
+    root = ET.fromstring(response.content)
+    news_items = []
+
+    for item in root.findall('./channel/item'):
+        news_dict = {
+            'title': item.find('title').text,
+            'link': item.find('link').text,
+            'pubDate': item.find('pubDate').text
+        }
+        news_items.append(news_dict)
+
+    with open(csv_filename, 'w', newline='', encoding='utf-8') as file:
+        writer = csv.DictWriter(file, fieldnames=['title', 'link', 'pubDate'])
+        writer.writeheader()
+        writer.writerows(news_items)
+
+    print(f"Google News内容已保存到 {csv_filename}")
+
+# 爬取维基百科页面
 scrape_wikipedia_page(
     "https://en.wikipedia.org/wiki/Israeli%E2%80%93Palestinian_conflict",
     "Wikipedia_English_version_of_the_Palestinian-Israeli_conflict_yifan.csv"
 )
-
-# 爬取中文版本的页面
 scrape_wikipedia_page(
-    "https://zh.wikipedia.org/wiki/%E4%BB%A5%E5%B7%B4%E5%86%B2%E7%AA%81",
+    "https://zh.wikipedia.org/zh-cn/%E4%BB%A5%E5%B7%B4%E5%86%B2%E7%AA%81",
     "Wikipedia_Chinese_version_of_the_Palestinian-Israeli_conflict_yifan.csv"
+)
+
+# 保存Google News RSS feed新闻
+save_news_to_csv(
+    'https://news.google.com/rss/search?q=%E5%B7%B4%E4%BB%A5%E5%86%B2%E7%AA%81&hl=zh-CN&gl=CN&ceid=CN:zh-Hans',
+    'Chinese_version_of_Google_News_crawling_yifan.csv'
+)
+save_news_to_csv(
+    'https://news.google.com/rss/search?q=Israeli-Palestinian+conflict&hl=en-US&gl=US&ceid=US:en',
+    'English_version_of_Google_News_crawling_yifan.csv'
 )
